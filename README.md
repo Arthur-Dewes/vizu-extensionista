f95c30cf159167e4ba124f92c876063688b22458
Arthur-Dewes <arthurgdewes@gmail.com>
2026-05-15 18:30:40 -0300

    sys and norm

---
+++ README.md
@@ -0,0 +1,4 @@
+### RUN:
+```bash
+uv run main.py "data/LEM 2021.xlsx" "data/LEM 2022 (1).xlsx" "data/LEM 2023.xlsx" "data/LEM 2024 (1).xlsx" "data/LEM 2025 1.xlsx"
+```
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import sys
 import pandas as pd
+<<<<<<< Updated upstream
 import plotly.express as px
+=======
+import unicodedata
+
+def normalize_text(index: pd.Index) -> pd.Index:
+    def _normalize(s: str) -> str:
+        nfd = unicodedata.normalize('NFD', s)
+        return ''.join(c for c in nfd if unicodedata.category(c) != 'Mn').upper()
+    return index.map(_normalize)
+>>>>>>> Stashed changes
 
 def read(path: str) -> pd.DataFrame:
     data: pd.DataFrame = pd.read_excel(path, header=None, skiprows=2, usecols=range(13), nrows=41)    
@@ -17,11 +28,14 @@ def read(path: str) -> pd.DataFrame:
         indexes[pos_matriculados] = f"{indexes[pos_matriculados]}{ed}"
         indexes[pos_aguardando] = f"{indexes[pos_aguardando]}{ed}"
         data.index = indexes    
+    data.index = normalize_text(data.index)
     data.index.name = "index"
     data.fillna(0, inplace=True)
+
     return data
 
 if __name__ == '__main__':
+<<<<<<< Updated upstream
     data = {
         2021: read("data/LEM 2021.xlsx"),
         2022: read("data/LEM 2022 (1).xlsx"),
@@ -35,3 +49,16 @@ if __name__ == '__main__':
         print(df.shape)
 
     print(data[2021])
+=======
+    if len(sys.argv) < 2:
+        sys.exit(1)
+
+    paths = sys.argv[1:]
+    dataframes = [read(path) for path in paths]
+
+    ref = dataframes[0].shape
+    for i, df in enumerate(dataframes):
+        assert df.shape == ref, f"Erro na tabela da posição {i}: esperado {ref}, mas obteve {df.shape}"
+
+    print(dataframes[0])
+>>>>>>> Stashed changes
