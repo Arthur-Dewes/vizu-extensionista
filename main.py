6335856b7a20f40559939bff34c5f7411bc4691d
Arthur-Dewes <arthurgdewes@gmail.com>
2026-05-15 18:32:51 -0300

    fix

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import sys
 import pandas as pd
-<<<<<<< Updated upstream
-import plotly.express as px
-=======
 import unicodedata
 
 def normalize_text(index: pd.Index) -> pd.Index:
@@ -10,7 +7,6 @@ def normalize_text(index: pd.Index) -> pd.Index:
         nfd = unicodedata.normalize('NFD', s)
         return ''.join(c for c in nfd if unicodedata.category(c) != 'Mn').upper()
     return index.map(_normalize)
->>>>>>> Stashed changes
 
 def read(path: str) -> pd.DataFrame:
     data: pd.DataFrame = pd.read_excel(path, header=None, skiprows=2, usecols=range(13), nrows=41)    
@@ -35,21 +31,6 @@ def read(path: str) -> pd.DataFrame:
     return data
 
 if __name__ == '__main__':
-<<<<<<< Updated upstream
-    data = {
-        2021: read("data/LEM 2021.xlsx"),
-        2022: read("data/LEM 2022 (1).xlsx"),
-        2023: read("data/LEM 2023.xlsx"),
-        2024: read("data/LEM 2024 (1).xlsx"),
-        2025: read("data/LEM 2025 1.xlsx")
-    }
-    
-    sum_per_column = {}
-    for df in data.values():
-        print(df.shape)
-
-    print(data[2021])
-=======
     if len(sys.argv) < 2:
         sys.exit(1)
 
@@ -61,4 +42,3 @@ if __name__ == '__main__':
         assert df.shape == ref, f"Erro na tabela da posição {i}: esperado {ref}, mas obteve {df.shape}"
 
     print(dataframes[0])
->>>>>>> Stashed changes
